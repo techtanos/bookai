f69a5d2527a360e40d71ccfc776c17ac56d1364e
techtanos <ftertanos@gmail.com>
2026-06-09 13:02:19 +0200

    list indexing and dictionary practice

---
+++ indexing.py
@@ -0,0 +1,10 @@
+   #List indexing
+drones = ["phontom", "mavic", "spark"]
+print(drones[0])
+print(drones[1])
+print(drones[2])
+print(drones[-1])
+   #Dictionary access
+drone = {"payload": 2.5, "speed": 10, "time": 20}
+print(drone["payload"])
+print(drone["speed"])
